59b721814a9ae66bc174848ec20c4b93f12a803f
felipedeluna <fcoladodeluna@gmail.com>
2023-07-25 14:29:05 -0300

    Populate nos questionários

--- scripts/models/questionario_html.js
+++ scripts/models/questionario_html.js
@@ -1,38 +1,107 @@
 const HTML = [
     {
-        pergunta: "Para que serve o HTML",
-        respostas:[
+        pergunta: "Para que serve o HTML?",
+        respostas: [
             "marcação",
             "estilização",
             "dinamismo",
             "tratar dados",
-        ]
+        ],
     },
     {
-        pergunta: "Para que serve o HTML",
-        respostas:[
-            "marcação",
-            "estilização",
-            "dinamismo",
-            "tratar dados",
-        ]
+        pergunta: "Quais são as tags usadas para criar uma tabela em HTML?",
+        respostas: [
+            "&lt;table&gt;&lt;/table&gt;",
+            "&lt;tr&gt;&lt;/tr&gt;",
+            "&lt;td&gt;&lt;/td&gt;",
+            "&lt;th&gt;&lt;/th&gt;",
+        ],
     },
     {
-        pergunta: "Para que serve o HTML",
-        respostas:[
-            "marcação",
-            "estilização",
-            "dinamismo",
-            "tratar dados",
-        ]
-    }
-
-]
+        pergunta: "Qual é a tag usada para criar um link em HTML?",
+        respostas: [
+            "&lt;link&gt;",
+            "&lt;a&gt;",
+            "&lt;href&gt;",
+            "&lt;url&gt;",
+        ],
+    },
+    {
+        pergunta: "O que significa a sigla HTML?",
+        respostas: [
+            "HyperText Markup Language",
+            "High Tech Machine Learning",
+            "Hyperlinks and Text Markup Language",
+            "Home Tool Markup Language",
+        ],
+    },
+    {
+        pergunta: "Qual é a versão mais recente do HTML?",
+        respostas: [
+            "HTML5",
+            "HTMLX",
+            "HTML2023",
+            "HTMLNext",
+        ],
+    },
+    {
+        pergunta: "Quais são as tags usadas para criar listas não ordenadas e ordenadas em HTML?",
+        respostas: [
+            "&lt;ul&gt; e &lt;ol&gt;",
+            "&lt;li&gt; e &lt;ol&gt;",
+            "&lt;ul&gt; e &lt;li&gt;",
+            "&lt;list&gt; e &lt;ordered&gt;",
+        ],
+    },
+    {
+        pergunta: "Qual é a tag usada para inserir uma imagem em uma página HTML?",
+        respostas: [
+            "&lt;img&gt;",
+            "&lt;src&gt;",
+            "&lt;picture&gt;",
+            "&lt;image&gt;",
+        ],
+    },
+    {
+        pergunta: "O que é um elemento &lt;div&gt; em HTML?",
+        respostas: [
+            "Uma divisão genérica que pode ser usada para agrupar outros elementos",
+            "Um elemento que define uma lista ordenada",
+            "Um tipo especial de link",
+            "Uma forma de estilização de texto",
+        ],
+    },
+    {
+        pergunta: "Qual é o elemento usado para definir cabeçalhos em HTML?",
+        respostas: [
+            "&lt;header&gt;",
+            "&lt;h1&gt;",
+            "&lt;heading&gt;",
+            "&lt;h6&gt;",
+        ],
+    },
+    {
+        pergunta: "Como é feito um comentário em HTML?",
+        respostas: [
+            "&lt;!-- Comentário --&gt;",
+            "// Comentário",
+            "/* Comentário */",
+            "' Comentário",
+        ],
+    },
+];
 
 const respostas_html = [
     "marcação",
-    "estilização",
-    "dinamismo",
+    "&lt;table&gt;&lt;/table&gt;",
+    "&lt;link&gt;",
+    "HyperText Markup Language",
+    "HTML5",
+    "&lt;ul&gt; e &lt;ol&gt;",
+    "&lt;img&gt;",
+    "Uma divisão genérica que pode ser usada para agrupar outros elementos",
+    "&lt;header&gt;",
+    "&lt;!-- Comentário --&gt;"
 ]
 
 export {HTML, respostas_html}
